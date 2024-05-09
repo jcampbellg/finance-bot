f3c5607dceaad439d2388a7ce9655c8457b3c710
Juan Carlos <jc.campbellg@gmail.com>
2024-05-09 09:48:45 -0600

    inverse budget /

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,7 @@ const bot = new TelegramBot(token, { polling: true })
 bot.on('message', async (msg) => {
   const today = dayjs().tz(process.env.timezone).format('YYYY-MM-DD HH:mm:ss')
   if (!userWhitelist.includes(msg.chat.id)) {
-    bot.sendMessage(msg.chat.id, 'You are not authorized to use this bot.')
+    bot.sendMessage(msg.chat.id, `You, ${msg.chat.id} are not authorized to use this bot.`)
     return
   }
 
@@ -280,7 +280,7 @@ bot.on('message', async (msg) => {
 
     const transString = allTrans.map(t => `${dayjs(t.date).format('D MMM')} <b>${t.description}</b>:\n${numeral(t.hnl || t.usd || 0).format('0,0.00')} ${t.hnl ? 'HNL' : 'USD'}${t.notes ? `<blockquote>${t.notes}</blockquote>` : ''}`).join('\n\n')
 
-    bot.sendMessage(msg.chat.id, `<b>${catToLook.name.toUpperCase()}:</b>\n${allTrans.length} Transaction${allTrans.length > 1 && 's'}\n\n${transString}\n\n<b>TOTAL HNL: ${totalHNL}\nTOTAL USD: ${totalUSD}</b>\n\nBudeget ${catToLook.coin}: ${numeral(catToLook.budget).format('0,0.00')} / ${numeral(catToLook.expenses).format('0,0.00')}`, { parse_mode: 'HTML' })
+    bot.sendMessage(msg.chat.id, `<b>${catToLook.name.toUpperCase()}:</b>\n${allTrans.length} Transaction${allTrans.length > 1 && 's'}\n\n${transString}\n\n<b>TOTAL HNL: ${totalHNL}\nTOTAL USD: ${totalUSD}</b>\n\nBudget ${catToLook.coin}: ${numeral(catToLook.expenses).format('0,0.00')} / ${numeral(catToLook.budget).format('0,0.00')}`, { parse_mode: 'HTML' })
     return
   }
 
