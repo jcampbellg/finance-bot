a5b8a03b9bfb8f0b6765f53800e95fc3e322f1f1
Juan Carlos <jc.campbellg@gmail.com>
2024-05-14 08:51:27 -0600

    negative value

--- src/index.ts
+++ src/index.ts
@@ -343,7 +343,7 @@ bot.on('message', async (msg) => {
       content: 'The date will be in the format YYYY-MM-DD HH:mm:ss. If no date is provided, use the current date.'
     }, {
       role: 'system',
-      content: 'The amount can also have negative values. If the user says "menos", "deposito" "ingreso", it means it is a negative value.'
+      content: 'If the user says "ingreso", make the amount a negative value.'
     }, {
       role: 'system',
       content: 'The coins accepted are USD and HNL. L also means HNL. Lempiras also mean HNL. Dollars also means USD.'
