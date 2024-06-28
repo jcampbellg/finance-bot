6d80144f846493f6ae13c52667b347dfaa3707c6
Juan Carlos <jc.campbellg@gmail.com>
2024-06-28 12:13:20 -0600

    allow dots

--- src/utils/isValid.ts
+++ src/utils/isValid.ts
@@ -22,7 +22,7 @@ export const isCurrencyValid = (text: string) => z.string().regex(/[a-zA-Z]+/).l
 
 export const isKeyValid = (text: string) => z.string().uuid().safeParse(text)
 
-export const isMathValid = (text: string) => z.string().regex(/^[0-9+\-*/ ]+$/).safeParse(text)
+export const isMathValid = (text: string) => z.string().regex(/^[0-9.+\-*/ ]+$/).safeParse(text)
 
 export const isDateValid = (text: string) => z.string().regex(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}/).safeParse(text)
 
